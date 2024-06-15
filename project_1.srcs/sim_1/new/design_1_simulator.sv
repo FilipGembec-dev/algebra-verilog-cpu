c49e3c0313258f86fa6814baea96e9f9d01cd1d6
Verilog Enjoyer <77166562+FilipGembec-dev@users.noreply.github.com>
2024-06-15 20:21:45 +0200

    Added AXI
    Added an AXI-4 lite master port with read and write operations on data memory operations with address index over the value in memory address space parameter.

---
+++ project_1.srcs/sim_1/new/design_1_simulator.sv
@@ -0,0 +1,24 @@
+`timescale 1ns / 1ps
+
+
+module design_1_simulator;
+
+  logic reset = 0;
+  logic sys_clock = 0;
+
+  design_1 design_1_i
+       (.reset(reset),
+        .sys_clock(sys_clock));
+
+//generate a system clock
+always begin
+     sys_clock <= ~sys_clock; #10;
+end
+
+
+initial begin
+  
+end
+
+
+endmodule
